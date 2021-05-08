8652741d5008428f8181d1d2cb5973d84cde739b
Guilherme Tassinari <guitassinari@gmail.com>
2021-05-08 09:46:49 -0300

    Create view and queries (#2)
    * Create view
    
    * Add some queries

---
+++ db/03-view.sql
@@ -0,0 +1,9 @@
+CREATE VIEW ContentPerception AS 
+  SELECT Content_.contentId, title, consumedAt, evaluatedAt, avg(rate) as avgRate, count(Comment_) as commentCount FROM Content_
+  INNER JOIN Consumption
+  ON Content_.contentId = Consumption.contentId
+  JOIN Evaluation
+  ON Content_.contentId = Evaluation.contentId
+  JOIN Comment_
+  ON Comment_.contentId = Content_.contentId
+  GROUP BY Content_.contentId, consumedAt, evaluatedAt, title;
