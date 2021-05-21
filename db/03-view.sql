458a1c938e5c8e883815301c4f72f1865b90853d
LeDSantos <d.santos.leti@gmail.com>
2021-05-21 01:22:28 -0300

    view deleted

--- db/03-view.sql
+++ db/03-view.sql
@@ -7,17 +7,4 @@ CREATE VIEW ContentPerception AS
   JOIN Comment_
   ON Comment_.contentId = Content_.contentId
   GROUP BY Content_.contentId, consumedAt, evaluatedAt, title;
-
-CREATE VIEW Accesses AS  --atividade dos alunos
-  SELECT name_, consumedAt AS data, 'consumo' AS atividade
-  FROM Student NATURAL JOIN Consumption
-  UNION
-  SELECT name_, commentedAt AS data, 'comentario' AS atividade
-  FROM Student NATURAL JOIN Comment_
-  UNION
-  SELECT name_, evaluatedAt AS data, 'avaliacao' AS atividade
-  FROM Student NATURAL JOIN Evaluation
-  UNION
-  SELECT name_, submitedAt AS data, 'redacao' AS atividade
-  FROM Student NATURAL JOIN Essay
-  ORDER BY name_, data;
+  
