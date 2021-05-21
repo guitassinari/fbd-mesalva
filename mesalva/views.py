d5c4e924fd23c6e25d7f144bfe6e66e733f210c3
LeDSantos <d.santos.leti@gmail.com>
2021-05-20 22:47:51 -0300

    New views pages with link to home

--- mesalva/views.py
+++ mesalva/views.py
@@ -11,11 +11,11 @@ def inactive_users(request):
   cursor.execute('''SELECT studentId, name_ FROM Student s
                       WHERE NOT EXISTS (SELECT 1 FROM Consumption c WHERE c.studentId = s.studentId);''')
 
-  msg="<h1>Usuários inativos</h1>"
+  msg="<title>Usuários inativos</title><center><h1>Usuários inativos</h1>"
 
   records = cursor.fetchall()
   
-  msg+="<table border=1><tr><th>studentId</th><th>name_ student</th></tr><tr>"
+  msg+="<table border=1><tr><th>studentId</th><th>name_ student</th></tr>"
   for row in records:
     msg+="<tr>"
     msg+="<td>"+str(row[0])+"</td>"
@@ -24,7 +24,7 @@ def inactive_users(request):
 
   msg+="</table>"
 
-  msg+="<p>"+str(len(records))+" registros encontrados.</p>"
+  msg+="<p>"+str(len(records))+" registros encontrados.</p><a href=\"/\">Home</a></center>"
 
   return HttpResponse(msg)
 
@@ -34,9 +34,24 @@ def top_commented_contents(request):
                       WHERE commentCount = (
                         SELECT MAX(commentCount) FROM ContentPerception
                       )
-                      LIMIT 1;''')
-  rows = cursor.fetchone()
-  return HttpResponse(rows)
+                      LIMIT 10;''')
+
+  msg="<title>TOP 10 conteúdos mais comentados</title><center><h1>TOP 10 conteúdos mais comentados</h1>"
+
+  records = cursor.fetchall()
+  
+  msg+="<table border=1><tr><th>Título</th><th>Número de comentários</th></tr>"
+  for row in records:
+    msg+="<tr>"
+    msg+="<td>"+row[0]+"</td>"
+    msg+="<td>"+str(row[1])+"</td>"
+    msg+="</tr>"
+
+  msg+="</table>"
+
+  msg+="<p>"+str(len(records))+" registros encontrados.</p><a href=\"/\">Home</a></center>"
+
+  return HttpResponse(msg)
 
 
 def top_rated_content(request):
@@ -45,6 +60,21 @@ def top_rated_content(request):
                       WHERE avgRate = (
                         SELECT MAX(avgRate) FROM ContentPerception
                       )
-                      LIMIT 1;''')
-  rows = cursor.fetchall()
-  return HttpResponse(rows)
+                      LIMIT 10;''')
+
+  msg="<title>TOP 10 conteúdos melhor avaliados</title><center><h1>TOP 10 conteúdos melhor avaliados</h1>"
+
+  records = cursor.fetchall()
+  
+  msg+="<table border=1><tr><th>Título</th><th>Média de avaliação</th></tr>"
+  for row in records:
+    msg+="<tr>"
+    msg+="<td>"+row[0]+"</td>"
+    msg+="<td>"+"{0:.2f}".format(row[1])+"</td>"
+    msg+="</tr>"
+
+  msg+="</table>"
+
+  msg+="<p>"+str(len(records))+" registros encontrados.</p><a href=\"/\">Home</a></center>"
+
+  return HttpResponse(msg)
