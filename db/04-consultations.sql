75616d12daa1b374397f9e018181f33cdea8d1a9
Guilherme Tassinari <guitassinari@gmail.com>
2021-05-21 19:34:54 -0300

    put view and queries together

--- db/04-consultations.sql
+++ db/04-consultations.sql
@@ -1,9 +1,17 @@
-------FALTA:
----GROUP BY com HAVING
----NOT EXISTS para responder questões do tipo TODOS ou NENHUM que < referencia >
----utilizar visão
+-- Percepção de conteúdo pelos usuários. Exibe o título, data de consumo,
+-- data de avaliação, média de avaliação e contagem de comentários de um conteúdo
+CREATE VIEW ContentPerception AS 
+  SELECT Content_.contentId, title, consumedAt, evaluatedAt, avg(rate) as avgRate, count(Comment_) as commentCount FROM Content_
+  INNER JOIN Consumption
+  ON Content_.contentId = Consumption.contentId
+  JOIN Evaluation
+  ON Content_.contentId = Evaluation.contentId
+  JOIN Comment_
+  ON Comment_.contentId = Content_.contentId
+  GROUP BY Content_.contentId, consumedAt, evaluatedAt, title;
+  
 
--- avaliações de redação
+-- avaliações de redaçãopor um professor
 SELECT name_, title, theme, comment_, rate, teachername
 FROM Student NATURAL JOIN Essay NATURAL JOIN EssayEvaluation;
 
@@ -12,19 +20,16 @@ SELECT name_, AVG(rate) AS avg_rate
 FROM Student NATURAL JOIN Essay NATURAL JOIN EssayEvaluation
 GROUP BY name_;
 
---alunos sem redações avaliadas
+-- alunos sem redações avaliadas
 SELECT name_
 FROM Student
 WHERE studentId NOT IN (SELECT studentId FROM Essay NATURAL JOIN EssayEvaluation);
 
---alunos que não enviaram redações, podem ser usuários q não conhecem essa função, daí pode mandar um email explicando como funciona
---SELECT name_
---FROM Student
---WHERE studentId NOT IN (SELECT studentId FROM Essay); 
-
---SELECT *
---FROM Plan NATURAL JOIN Product JOIN Course USING (courseId) JOIN CourseModule USING (courseId) JOIN Module_ USING (moduleId) NATURAL JOIN --ModuleContent JOIN Content_ USING (contentId)
---WHERE free_='true';
+--alunos que não enviaram redações, podem ser usuários q não conhecem essa função,
+-- daí pode mandar um email explicando como funciona
+SELECT name_
+FROM Student
+WHERE studentId NOT IN (SELECT studentId FROM Essay); 
 
 --Produtos com content_ pago
 SELECT Product.name_
@@ -33,7 +38,7 @@ WHERE free_='false'
 GROUP BY Product.name_;
 
 --Conteúdos gratuitos
---SELECT title, description FROM Content_ WHERE free_='true';
+SELECT title, description FROM Content_ WHERE free_='true';
 
 -- exercicios disponíveis
 SELECT exerciseId, difficulty, Question.name_, Option_.name_
@@ -52,17 +57,17 @@ FROM Exercise JOIN Question USING (exerciseId) JOIN Option_ USING (questionId)
 WHERE exerciseId=1 AND correct='true';
 
 -- numero respostas corretas de cada aluno
---SELECT Student.name_, COUNT(DISTINCT questionId) AS acertos
---FROM Student NATURAL JOIN QuestionAnswer JOIN Option_ USING (optionId)
---WHERE correct='true'
---GROUP BY Student.name_
---ORDER BY Student.name_;
+SELECT Student.name_, COUNT(DISTINCT questionId) AS acertos
+FROM Student NATURAL JOIN QuestionAnswer JOIN Option_ USING (optionId)
+WHERE correct='true'
+GROUP BY Student.name_
+ORDER BY Student.name_;
 
 --numero de questões distintas respondidas por cada aluno
---SELECT Student.name_, COUNT(DISTINCT questionId) AS questoes_respondidas
---FROM Student LEFT JOIN QuestionAnswer USING (studentId) LEFT JOIN Option_ USING (optionId)
---GROUP BY Student.name_
---ORDER BY Student.name_;
+SELECT Student.name_, COUNT(DISTINCT questionId) AS questoes_respondidas
+FROM Student LEFT JOIN QuestionAnswer USING (studentId) LEFT JOIN Option_ USING (optionId)
+GROUP BY Student.name_
+ORDER BY Student.name_;
 
 --numero de questões distintas respondidas e acertos por cada aluno
 SELECT Student.name_, COUNT(DISTINCT questionId) AS questoes_respondidas, COUNT(*) FILTER (WHERE correct) AS acertos
@@ -70,9 +75,6 @@ FROM Student LEFT JOIN QuestionAnswer USING (studentId) LEFT JOIN Option_ USING 
 GROUP BY Student.name_
 ORDER BY Student.name_;
 
---SELECT * FROM Exercise;
---SELECT * FROM Class_;
---SELECT * FROM ExtraMaterial;
 
 -- conteudo e atividades
 SELECT title AS Conteudo, COUNT(classId) AS Aulas, COUNT(exerciseId) AS Exercicios, COUNT(extramaterialId) AS MaterialExtra
@@ -81,11 +83,11 @@ GROUP BY title
 ORDER BY title;
 
 -- conteudos pagos
---SELECT title AS Conteudo, COUNT(classId) AS Aulas, COUNT(exerciseId) AS Exercicios, COUNT(extramaterialId) AS MaterialExtra
---FROM Content_ NATURAL LEFT JOIN Class_ NATURAL LEFT JOIN Exercise NATURAL LEFT JOIN ExtraMaterial
---GROUP BY title
---HAVING title = ANY (SELECT title FROM Content_ WHERE free_='false')
---ORDER BY title;
+SELECT title AS Conteudo, COUNT(classId) AS Aulas, COUNT(exerciseId) AS Exercicios, COUNT(extramaterialId) AS MaterialExtra
+FROM Content_ NATURAL LEFT JOIN Class_ NATURAL LEFT JOIN Exercise NATURAL LEFT JOIN ExtraMaterial
+GROUP BY title
+HAVING title = ANY (SELECT title FROM Content_ WHERE free_='false')
+ORDER BY title;
 
 -- conteudos gratuitos
 SELECT title AS Conteudo, COUNT(classId) AS Aulas, COUNT(exerciseId) AS Exercicios, COUNT(extramaterialId) AS MaterialExtra
@@ -94,11 +96,11 @@ WHERE free_='true'
 GROUP BY title
 ORDER BY title;
 
---comentarios
+-- comentarios
 SELECT title, commentedat, name_, text_
 FROM Content_ NATURAL JOIN Comment_ NATURAL JOIN Student;
 
---pedidos
+-- pedidos
 SELECT orderedAt, Student.name_ AS aluno, cardNumber, validThrough, Product.name_ AS produto
 FROM Order_ JOIN Product USING (productId) JOIN Student USING (studentId) JOIN CreditCard USING (creditcardId)
 ORDER BY orderedAt;
